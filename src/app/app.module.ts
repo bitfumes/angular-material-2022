f21c1723a7371bde02fc18e72a1bae63a1d019ed
sarthaksavvy <sarthak@bitfumes.com>
2022-08-29 12:55:49 +0530

    03 Angular Material - Install Angular Material

---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule()
+export class AppModule { }

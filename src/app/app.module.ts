40f9f722ecb2005d1789c4adf185eeeee5412483
sarthaksavvy <sarthak@bitfumes.com>
2022-08-29 13:03:35 +0530

    04 Angular Material - Use first button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button"
 
-@NgModule()
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
 export class AppModule { }

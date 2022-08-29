cc940d1fd1c63314fbb09742efef4cd1b0063234
sarthaksavvy <sarthak@bitfumes.com>
2022-08-29 23:48:12 +0530

    09 Angular Material - Input field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialDesignModule } from './material-design/material-design.module';
 import { LoginComponent } from './auth/login/login.component';
+import { MaterialDesignModule } from './material-design/material-design.module';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatButtonModule, MatCardModule, MatToolbarModule],
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+  ],
 })
 export class MaterialDesignModule {}

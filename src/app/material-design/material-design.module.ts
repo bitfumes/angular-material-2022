37391c4758d50da5c9a232ff4f22c320cba4aac0
sarthaksavvy <sarthak@bitfumes.com>
2022-08-30 13:41:53 +0530

    14 Angular Material - icon and badges

--- src/app/material-design/material-design.module.ts
+++ src/app/material-design/material-design.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -24,6 +26,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatProgressSpinnerModule,
     MatGridListModule,
     MatChipsModule,
+    MatIconModule,
+    MatBadgeModule,
   ],
 })
 export class MaterialDesignModule {}
